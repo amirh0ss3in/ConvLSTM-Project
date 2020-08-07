36cf8bb0a31ab3c4c7859240f3b5b2379b70e18a
amirh0ss3in <59564278+amirh0ss3in@users.noreply.github.com>
2020-08-07 15:14:06 +0430

    Update fashion_mnist .py

--- fashion_mnist .py
+++ fashion_mnist .py
@@ -1,5 +1,5 @@
 'Developed by Amirhossein Rezaei'
-#Maximum validation accuracy: 91.38% (on 15 epochs; withoun any image processing)
+#Maximum validation accuracy: 91.38% (on 15 epochs; without any image processing)
 #V 1.0.0 , hyperparameter tuning is needed.
 
 
