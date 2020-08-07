f72098f4e2b6e31ee7f060ddf41d32689f43af58
amirh0ss3in <59564278+amirh0ss3in@users.noreply.github.com>
2020-08-07 15:14:47 +0430

    Update noisy_dataset.py

--- noisy_dataset.py
+++ noisy_dataset.py
@@ -1,5 +1,5 @@
 'Developed by Amirhossein Rezaei'
-#Maximum validation accuracy: 98.97% (on 15 epochs; withoun any image processing)
+#Maximum validation accuracy: 98.97% (on 15 epochs; without any image processing)
 #On this dataset, the ratio between training and test data is 2:1.
 #This result was obtaind with 50,000 training images and 25,000 testing images. 
 #V 1.0.0 , hyperparameter tuning is needed.
